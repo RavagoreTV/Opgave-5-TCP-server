781285650deb3d3154d9951ff6f8997c3809329a
Victor Dahl Saltoft <vict2729@edu.easj.dk>
2021-10-04 11:18:09 +0200

    Done

--- Opgave 5 TCP-server/Program.cs
+++ Opgave 5 TCP-server/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Opgave_1_Csharp;
 
@@ -9,11 +11,16 @@ namespace Opgave_5_TCP_server
 {
     class Program
     {
+        public static List<FootballPlayer> footballPlayers = new List<FootballPlayer>
+        {
+            new FootballPlayer {Id = 1, Name = "Benjamin", Price = 3000000, ShirtNumber = 14},
+            new FootballPlayer {Id = 2, Name = "Peter", Price = 250, ShirtNumber = 10}
+        };
         static void Main(string[] args)
         {
             Console.WriteLine("TCP-Server is startet");
 
-            TcpListener listener = new TcpListener(IPAddress.Any, port: 10002);
+            TcpListener listener = new TcpListener(IPAddress.Any, port: 2121);
             listener.Start();
 
             while (true)
@@ -33,6 +40,44 @@ namespace Opgave_5_TCP_server
             StreamReader reader = new StreamReader(ns);
             StreamWriter writer = new StreamWriter(ns);
             string message = reader.ReadLine();
+            string messageID = reader.ReadLine();
+
+            writer.WriteLine("Valgmuligheder for denne server er HA for hent alle, HID for hent for fodboldspillerns id eller G for at gemme");
+
+            if(message.ToUpper().StartsWith("HA"))
+            {
+                if(footballPlayers != null)
+                {
+                    foreach (var players in footballPlayers)
+                    {
+                        writer.WriteLine($"Spillersnavn: {players.Name} Spillers pris: {players.Price} Spillers trøjenummer: {players.ShirtNumber}");
+                        writer.Flush();
+                    }
+                }   
+            } else if(message.ToUpper() == "HID")
+            {
+                int id = -1;
+                if(int.TryParse(messageID, out id))
+                {
+                    foreach(var players in footballPlayers)
+                    {
+                        if(players.Id == id)
+                        {
+                            writer.WriteLine($"Id på den søgte spiller: {players.Id}, Name: {players.Name}, Price: {players.Price}, Shirtnumber: {players.ShirtNumber}");
+                            writer.Flush();
+                        }
+                    }
+                }
+            }else if(message.ToUpper().StartsWith("G"))
+            {
+                FootballPlayer player = new FootballPlayer();
+                footballPlayers.Add(player);
+                player = JsonSerializer.Deserialize<FootballPlayer>(messageID);
+
+                writer.WriteLine("Spiller gemt");
+                writer.Flush();
+            }
+            sockert.Close();
         }
      }
 }
